6cd02aa22e622350f3239fb2cb5977459e62e35a
KappaRoss1 <71713872+KappaRoss1@users.noreply.github.com>
2022-04-09 00:46:38 +0200

    yep

--- excersices/2/CommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/commonasteroids/Asteroid.java
+++
@@ -1,9 +0,0 @@
-package dk.sdu.mmmi.cbse.commonasteroids;
-
-import dk.sdu.mmmi.cbse.common.data.Entity;
-
-/**
- * @author Dinh Phu
- */
-public class Asteroid extends Entity {
-}
--- excersices/2/CommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/commonasteroids/IAsteroidSplitter.java
+++
@@ -1,7 +0,0 @@
-package dk.sdu.mmmi.cbse.commonasteroids;
-
-import dk.sdu.mmmi.cbse.common.data.*;
-
-public interface IAsteroidSplitter {
-    void createSplitAsteroid(Entity entity, World world);
-}
